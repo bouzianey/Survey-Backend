f5870cbfc05e5161d2b11b3cc3f2866f0f2daa5d
bouzianey1 <yacinebouziane1@gmail.com>
2020-12-27 15:11:43 -0500

    Added URL config for heroku Postgres db

--- init.py
+++ init.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'something'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 

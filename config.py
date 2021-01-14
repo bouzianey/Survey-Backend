559b72c1b0dd4723f4a5a4d8c3a4cf4bdfc1fccd
bouzianey1 <yacinebouziane1@gmail.com>
2021-01-13 22:13:30 -0500

    Updated mail settings

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ ADMINS = [os.environ['EMAIL_USER']]
 
 mail_settings = {
     "MAIL_SERVER": 'smtp.gmail.com',
-    "MAIL_PORT": 587,
-    "MAIL_USE_TLS": True,
-    "MAIL_USE_SSL": False,
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
     "MAIL_USERNAME": username,
     "MAIL_PASSWORD": password
 }
